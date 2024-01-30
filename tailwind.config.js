c575b0fe52fe229ca82cdd4e4f9122cc8bbb8603
fliyyer <rahmatcube15@gmail.com>
2024-01-30 18:10:41 +0700

    setup complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-color': '#457EFF',
+        'darker': '#4064B4',
+        'subtitle' : '#F6F8FF',
+        'dark': '#5E5E5E',
+        'error' : '#FF3B3B',
+        'warning' : '#FFCC00',
+        'info' : '#0063F7',
+        'success' : '#06C270'
+
+      }
+    },
   },
   plugins: [],
 }
